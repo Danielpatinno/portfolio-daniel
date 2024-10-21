3f66c7c03819b878c5fc1c45f20b17dac4ada6ec
Daniel Patino <Neniade777@gmail.com>
2024-10-21 20:44:08 -0300

    testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
 import { Footer } from "@/components/home/footer";
 import { Skils } from "@/components/home/skils";
-
 import { ArrowDownToLine } from "lucide-react";
 import { Button } from "@/components/ui/button";
-
 import Image from "next/image";
-
 import daniel01 from "@/assets/daniel01.jpeg";
 
 export default function Home() {
@@ -14,21 +11,17 @@ export default function Home() {
       <section className="container mx-auto flex flex-col-reverse lg:flex-row justify-center items-center lg:justify-between lg:min-h-screen lg:py-8">
         <div className="w-full lg:w-1/2 text-center lg:text-left px-6 lg:px-12">
           <p className="text-3xl lg:text-6xl font-bold">Olá!, Eu sou o</p>
-
           <p className="font-bold text-4xl lg:text-6xl text-primary mt-2">
             Daniel Patino
           </p>
-
           <p className="text-2xl lg:text-3xl my-4 bg-clip-text text-transparent bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 animate-gradient-move">
             Desenvolvedor FrontEnd
           </p>
-
           <p className="text-base lg:text-lg mt-2">
             Eu desenvolvo websites usando React, TypeScript, Next.js, Tailwind
             CSS e outras tecnologias modernas para criar experiências web
             incríveis.
           </p>
-
           <Button className="mt-8">
             <a
               className="flex items-center"
@@ -40,7 +33,6 @@ export default function Home() {
             </a>
           </Button>
         </div>
-
         <div className="flex justify-center items-center w-full mt-6 lg:w-2/4 lg:h-[40rem] h-64 overflow-hidden">
           <div className="border-2 border-primary rounded-full w-64 h-64 lg:w-full lg:h-full">
             <Image
@@ -51,13 +43,11 @@ export default function Home() {
           </div>
         </div>
       </section>
-
       <section className="min-h-screen flex flex-col">
         <h1 className="w-full p-6 text-2xl text-center border-b border-primary">
           HABILIDADES
         </h1>
         <Skils />
-
         <Footer />
       </section>
     </main>
