72d36e88f9f4a8a2a31899c3ede9eedd4c596473
Daniel Patino <Neniade777@gmail.com>
2024-10-21 19:53:42 -0300

    New photos

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,59 +1,71 @@
 import { Footer } from "@/components/home/footer";
 import { MapPin } from "lucide-react";
+import Image from "next/image";
+import daniel02 from "@/assets/daniel02.jpeg";
 
 export default function About() {
   return (
-    <main>
-      <section className="flex flex-col sm:min-h-screen justify-center items-center">
-        <div className="text-center lg:mt-14 text-2xl">
-          <h1 className="mb-4">
-            Daniel
-            <span className="text-primary">Patino</span>
-          </h1>
-          <p className="flex gap-2 justify-center items-center text-xl">
-            <MapPin />
-            Maringá-PR
-          </p>
-        </div>
-        <div className="flex m-auto h-52 border-b">FOTO AQUI</div>
+<main>
+  <section className="flex flex-col sm:min-h-screen justify-center items-center">
+    <div className="text-center lg:mt-14 text-3xl lg:text-4xl">
+      <h1 className="mb-2 font-bold">
+        Daniel <span className="text-primary">Patino</span>
+      </h1>
+      <p className="flex gap-2 justify-center items-center text-lg lg:text-xl">
+        <MapPin className="text-primary" />
+        Maringá-PR
+      </p>
+    </div>
+    
+    <div className="flex justify-center items-center h-52 w-52 lg:h-72 lg:w-72 rounded-full overflow-hidden border-2 border-primary mb-4">
+      <Image
+        src={daniel02}
+        alt="Daniel"
+        className="w-full h-full object-cover"
+      />
+    </div>
 
-        <div className="container text-xl mt-2">
-          <p className="mb-4">
-            Sou um apaixonado por tecnologia, com foco em desenvolvimento
-            front-end. Atualmente, estou no terceiro ano de Engenharia de
-            Software e possuo uma sólida experiência em programação com React,
-            TypeScript, TailwindCSS e Styled-Components.
-          </p>
+    <div className="container max-w-3xl text-lg lg:text-xl mt-6 lg:mt-8 leading-relaxed px-4">
+      <p className="mb-4">
+        Sou um apaixonado por tecnologia, com foco em desenvolvimento front-end.
+        Atualmente, estou no terceiro ano de Engenharia de Software e possuo uma
+        sólida experiência em programação com React, TypeScript, TailwindCSS e Styled-Components.
+      </p>
 
-          <p className="mb-4">
-            Originalmente do Paraguai, nasci em Assunção e há dois anos resido
-            no Brasil, onde tenho aprimorado minhas habilidades e conhecimentos
-            na área de desenvolvimento web.
-          </p>
+      <p className="mb-4">
+        Originalmente do Paraguai, nasci em Assunção e há dois anos resido no Brasil,
+        onde tenho aprimorado minhas habilidades e conhecimentos na área de desenvolvimento web.
+      </p>
 
-          <p className="mb-4">
-            Minha jornada no mundo da programação começou com um interesse
-            genuíno em criar experiências digitais impactantes e intuitivas. Com
-            o tempo, desenvolvi uma forte afinidade com o front-end, onde posso
-            combinar minha criatividade e habilidades técnicas para construir
-            interfaces de usuário modernas e eficientes.
-          </p>
+      <p className="mb-4">
+        Minha jornada no mundo da programação começou com um interesse genuíno em criar
+        experiências digitais impactantes e intuitivas. Com o tempo, desenvolvi uma forte
+        afinidade com o front-end, onde posso combinar minha criatividade e habilidades
+        técnicas para construir interfaces de usuário modernas e eficientes.
+      </p>
 
-          <p className="mb-4">
-            Além do meu foco no front-end, tenho conhecimento em back-end com
-            Node.js, o que me permite compreender e colaborar efetivamente em
-            projetos full-stack.
-          </p>
+      <p className="mb-4">
+        Além do meu foco no front-end, tenho conhecimento em back-end com Node.js, o que
+        me permite compreender e colaborar efetivamente em projetos full-stack.
+      </p>
 
-          <p className="mb-4">
-            Estou sempre em busca de novos desafios e oportunidades para
-            aprender e crescer como desenvolvedor. Sinta-se à vontade para
-            explorar meus projetos e entrar em contato comigo para colaborações
-            ou para saber mais sobre meu trabalho.
-          </p>
-        </div>
-        <Footer />
-      </section>
-    </main>
+      <p className="mb-4">
+        Estou sempre em busca de novos desafios e oportunidades para aprender e crescer como
+        desenvolvedor. Sinta-se à vontade para explorar meus projetos e entrar em contato comigo
+        para colaborações ou para saber mais sobre meu trabalho.
+      </p>
+
+      <div className="flex justify-center mt-6">
+          <a href="/project" className="flex items-center">
+            Veja Meus Projetos
+          </a>
+     
+      </div>
+    </div>
+
+    <Footer />
+  </section>
+</main>
+
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,49 +4,64 @@ import { Skils } from "@/components/home/skils";
 import { ArrowDownToLine } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
+import Image from "next/image";
+
+import daniel01 from "@/assets/daniel01.jpeg";
+
+
 export default function Home() {
   return (
-    <main>
-      <section className="container mx-auto flex flex-col-reverse lg:flex-row justify-center lg:justify-around items-center lg:min-h-screen">
-        <div className="w-11/12 lg:w-1/2 mb-24 lg:mb-2 mt-8 lg:mx-2">
-          <p className="text-3xl lg:text-6xl lg:font-bold">Olá!, Eu sou o</p>
+<main>
+  <section className="container mx-auto flex flex-col-reverse lg:flex-row justify-center items-center lg:justify-between lg:min-h-screen lg:py-8">
+    <div className="w-full lg:w-1/2 text-center lg:text-left px-6 lg:px-12">
+      <p className="text-3xl lg:text-6xl font-bold">
+        Olá!, Eu sou o
+      </p>
 
-          <p className="font-bold text-3xl lg:text-4xl text-primary mt-2">
-            Daniel Patino
-          </p>
+      <p className="font-bold text-4xl lg:text-6xl text-primary mt-2">
+        Daniel Patino
+      </p>
 
-          <p className="text-2xl lg:text-3xl lg:my-4 bg-clip-text text-transparent bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 animate-gradient-move">
-            Desenvolvedor FrontEnd
-          </p>
+      <p className="text-2xl lg:text-3xl my-4 bg-clip-text text-transparent bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 animate-gradient-move">
+        Desenvolvedor FrontEnd
+      </p>
 
-          <p className="mt-2">
-            Eu desenvolvo websites usando React, TypeScript, Next.js, Tailwind
-            CSS e outras tecnologias modernas para criar experiências web
-            incríveis
-          </p>
+      <p className="text-base lg:text-lg mt-2">
+        Eu desenvolvo websites usando React, TypeScript, Next.js, Tailwind CSS e outras tecnologias modernas para criar experiências web incríveis.
+      </p>
 
-          <Button className="mt-8">
-            <a
-              className="flex items-center"
-              download="Currículo Daniel"
-              href="https://drive.google.com/file/d/1fzhmJwIzvc732y4BUPNDDl8KYPwhnAre/view?usp=drive_link"
-            >
-              <ArrowDownToLine /> Download Currículo
-            </a>
-          </Button>
-        </div>
+      <Button className="mt-8">
+        <a
+          className="flex items-center"
+          download="Currículo Daniel"
+          href="https://drive.google.com/file/d/1fzhmJwIzvc732y4BUPNDDl8KYPwhnAre/view?usp=drive_link"
+        >
+          <ArrowDownToLine className="mr-2" />
+          Download Currículo
+        </a>
+      </Button>
+    </div>
 
-        <div className="border-2 border-primary h-64 w-64 lg:w-1/2 lg:h-96 rounded-full"></div>
-      </section>
+    <div className="flex justify-center items-center w-full lg:w-2/4 lg:h-[37rem] h-64 overflow-hidden">
+      <div className="border-2 border-primary rounded-full w-64 h-64 lg:w-full lg:h-full">
+        <Image
+          src={daniel01}
+          alt="Daniel"
+          className="w-full h-full object-cover rounded-full"
+        />
+      </div>
+    </div>
+  </section>
 
-      <section className="min-h-screen flex flex-col">
-        <h1 className="w-full p-6 text-2xl text-center border-b border-primary ">
-          HABILIDADES
-        </h1>
-        <Skils />
+  <section className="min-h-screen flex flex-col">
+    <h1 className="w-full p-6 text-2xl text-center border-b border-primary">
+      HABILIDADES
+    </h1>
+    <Skils />
 
-        <Footer />
-      </section>
-    </main>
+    <Footer />
+  </section>
+</main>
+
   );
 }
