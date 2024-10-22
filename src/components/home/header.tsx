4964c7812e807346ace21d758fac0919a03006be
Daniel Patino <106635153+Danielpatinno@users.noreply.github.com>
2024-10-21 22:27:27 -0300

    Update header.tsx

--- src/components/home/header.tsx
+++ src/components/home/header.tsx
@@ -85,7 +85,7 @@ export function Header() {
             className="block w-full text-xl px-4 py-2 rounded-lg"
             onClick={closeMenu}
           >
-            SOBRE MIN
+            SOBRE
           </Link>
         </li>
         <li className="px-4 py-2 hover:bg-primary hover:text-white">
